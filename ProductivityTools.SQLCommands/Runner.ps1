46827068fd1749c10885621b28e8f919019a37f1
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-11-15 06:32:05 +0100

    rename

--- ProductivityTools.SQLCommands/Runner.ps1
+++ ProductivityTools.SQLCommands/Runner.ps1
@@ -4,7 +4,7 @@
 clear
 Write-Host "pawel"
 cd $PSScriptRoot
-Import-Module ".\ProductivityTools.PSSQLCommands.psm1" -Force
+Import-Module ".\ProductivityTools.SQLCommands.psm1" -Force
 Test-SQLDatabase -SqlInstance ".\sql2017" -DatabaseName "pawel"
 #New-SQLDatabase -SqlInstance ".\sql2014" -DatabaseName "paweltest123" -Path "d:\trash" -Verbose -Force
 New-SQLTable  -SqlInstance ".\sql2014" -DatabaseName "paweltest123" -TableName "table1" -Verbose -Force
